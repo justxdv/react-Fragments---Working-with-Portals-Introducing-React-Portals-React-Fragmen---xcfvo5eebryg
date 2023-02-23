53f4bfd2d4bfd82293a1754e9ac0413d51e50081
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-23 16:02:29 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React, { useState } from "react";
+import ListItems from "./ListItems";
 
- const List = ({listx}) =>{
-  return(
-    // code here
-    
-      <ListItems  />
-    
-    
-  )
-}
+const List = () => {
+  const [listItems, setListItems] = useState([]);
+
+  const handleButtonClick = () => {
+    const n = document.getElementById("input").value;
+    const items = [];
+
+    for (let i = 1; i <= n; i++) {
+      items.push(i);
+    }
+
+    setListItems(items);
+  };
+
+  return (
+    <>
+      <label htmlFor="input">Enter a number:</label>
+      <input type="number" id="input" />
+      <button id="button" onClick={handleButtonClick}>
+        Generate List
+      </button>
+      {listItems.length > 0 && (
+        <ul>
+          {listItems.map((item) => (
+            <React.Fragment key={item}>
+              <ListItems valuex={item} />
+            </React.Fragment>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default List;
